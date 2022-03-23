39d48fe4a21cad52d0b52dcc08e5cc82474f7d65
= <=>
2022-03-23 17:06:46 +0200

    game design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import React from "react"
+import "./App.css"
+import Die from "./components/Die"
 export default function App()
 {
+
+    const [dice,setDice] = React.useState(()=>newDice())
+
+
+    function newDice(){
+        const dice = [];
+
+        for(let i = 0 ; i <= 9 ; i++){
+            dice.push(Math.ceil(Math.random() * 6))
+        }
+        return dice;
+    }
+
+
+
+
+
+    const diceElements = dice.map((number,index) => {
+        return <Die key= {index} value={number} />
+    })
+
+
     return (
-        <h1>Tenzies App Game</h1>
+        <main>
+            <h2 className="title">Tenzies</h2>
+            <p className="description">
+                Roll until all dice are the same.
+                Click each die to freeze it at its current value between rolls.
+            </p>
+            <section className="dice">
+                {diceElements}
+            </section>
+            <button className="roll--btn">roll</button>
+        </main>
     )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,10 @@
+import "./Die.css"
+
+export default function Die(props)
+{
+    return (
+        <div className="die">
+            {props.value}
+        </div>
+    )
+}
